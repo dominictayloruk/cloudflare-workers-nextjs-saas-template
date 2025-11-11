bf5e38a934e1a180130764d9f29de146c17aa1d5
Dominic Taylor <dominic@dominictaylor.dev>
2025-11-11 16:44:01 +0000

    chore: update Stripe API version to 2025-10-29.clover

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -15,7 +15,7 @@ export function getStripe() {
   }
 
   stripeInstance = new Stripe(stripeSecretKey, {
-    apiVersion: "2025-09-30.clover",
+    apiVersion: "2025-10-29.clover",
     typescript: true,
     httpClient: Stripe.createFetchHttpClient(),
   });
